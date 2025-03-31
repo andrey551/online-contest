6d3d3290bded6537eff32286fd8d38cd4ecfa808
andrey551 <76159085+andrey551@users.noreply.github.com>
2025-03-31 23:38:25 +0300

    Refactor: Remove comment and contest service

--- contest/src/main/java/com/tad/contest/dto/wrapper/WrapperContestResponse.java
+++
@@ -1,9 +0,0 @@
-package com.tad.contest.dto.wrapper;
-
-import com.tad.contest.dto.object.ContestResponseDTO;
-import com.tad.contest.model.enums.TransactionStatus;
-
-public record WrapperContestResponse(
-        TransactionStatus status,
-        ContestResponseDTO response
-) {}
