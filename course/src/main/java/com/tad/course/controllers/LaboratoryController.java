14d2d581c8103be08781c58336b90707d0ed361d
andrey551 <76159085+andrey551@users.noreply.github.com>
2025-05-05 22:52:05 +0300

    Refactor: Rename wrapper for /GET laboratories response

--- course/src/main/java/com/tad/course/controllers/LaboratoryController.java
+++ course/src/main/java/com/tad/course/controllers/LaboratoryController.java
@@ -14,6 +14,7 @@ import static com.tad.course.mapper.LaboratoryMapper.toHttpResponse;
 
 @RestController
 @RequestMapping("/api/v1/laboratories")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class LaboratoryController {
 
     @Autowired
